359cf6d02dcd661bebcefb95747d301207c8d509
Flowdev Teams <haikalsialip999@gmail.com>
2026-01-22 21:30:16 +0700

    update beranda modern

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -61,6 +61,7 @@ export function Navigation() {
   return (
     <>
       <header
+      
         className={cn(
           "fixed top-0 left-0 right-0 z-50 transition-all duration-300",
           isScrolled || isOpen
